4d8af237ff6214cac02e173a802f878abf295a25
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-03-07 16:16:07 -0500

    add peering

--- main.tf
+++ main.tf
@@ -21,3 +21,18 @@ resource "hcp_vault_cluster" "cluster" {
     prevent_destroy = true
   }
 }
+
+resource "hcp_aws_network_peering" "peering" {
+  hvn_id          = hcp_hvn.hvn.hvn_id
+  peering_id      = "vault-peering"
+  peer_vpc_id     = "vpc-0b70cc11123de0961"
+  peer_account_id = "590184029125"
+  peer_vpc_region = "us-west-2"
+}
+
+resource "hcp_hvn_route" "route" {
+  hvn_link         = hcp_hvn.hvn.self_link
+  hvn_route_id     = "vault-route"
+  destination_cidr = "172.31.0.0/16"
+  target_link      = hcp_aws_network_peering.peering.self_link
+}
