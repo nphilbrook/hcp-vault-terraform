e4acf981619562bfe705a97e32211ba11b70a400
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-03-10 14:48:48 -0400

    remove spaces in BU Vault Lab project

--- vault_auth.tf
+++ vault_auth.tf
@@ -21,7 +21,7 @@ resource "vault_jwt_auth_backend_role" "hcp_tf_admin" {
 
   bound_audiences = ["vault.workload.identity"]
   bound_claims = {
-    sub = "organization:philbrook:project:*:workspace:hcp-vault-tf:run_phase:*"
+    sub = "organization:philbrook:project:bu-vault-lab:workspace:hcp-vault-tf:run_phase:*"
   }
   user_claim = "terraform_full_workspace"
   role_type  = "jwt"
