5647a3bc8c7d465051c5348c816b396e9b130501
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-03-18 12:47:24 -0400

    use attribute reference

--- app_policies.tf
+++ app_policies.tf
@@ -1,6 +1,6 @@
 data "vault_policy_document" "probable_pancake" {
   rule {
-    path         = "aws/creds/probable-pancake-tf"
+    path         = "aws/creds/${vault_aws_secret_backend_role.probable_pancake_role.name}"
     capabilities = ["read"]
     description  = "Read dynamic AWS credentials for the specified role"
   }
