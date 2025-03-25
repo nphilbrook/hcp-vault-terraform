5dd920bf22e8066c89037ffeceeffdcbbd8998af
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-03-25 16:44:10 -0400

    Create top level namespace JWT roles and policies

--- locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  top_level_namespaces = toset([
-    "testing",
-    "live"
-  ])
-}
--- namespaces.tf
+++ namespaces.tf
@@ -1,3 +1,10 @@
+locals {
+  top_level_namespaces = toset([
+    "testing",
+    "live"
+  ])
+}
+
 resource "vault_namespace" "top_level_namespaces" {
   for_each = local.top_level_namespaces
   path     = each.value
