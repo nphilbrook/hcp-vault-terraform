7a323186689e2a62d658f7beefe09a1467f64e99
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-01 16:13:41 -0400

    try to hook up external groups

--- saml_auth.tf
+++ saml_auth.tf
@@ -1,5 +1,9 @@
+locals {
+  saml_mount = "saml"
+}
+
 resource "vault_saml_auth_backend" "auth0" {
-  path             = "saml"
+  path             = local.saml_mount
   idp_metadata_url = "https://dev-duhq6zvtxhjtgss0.us.auth0.com/samlp/metadata/djIEvjayGHtOJsVaaZwOWhiHvQkmaSob"
   entity_id        = "${hcp_vault_cluster.cluster.vault_private_endpoint_url}/v1/auth/saml"
   acs_urls         = ["${hcp_vault_cluster.cluster.vault_private_endpoint_url}/v1/admin/auth/saml/callback"]
@@ -7,7 +11,25 @@ resource "vault_saml_auth_backend" "auth0" {
 }
 
 resource "vault_saml_auth_backend_role" "default" {
-  path           = vault_saml_auth_backend.auth0.path
-  name           = "default"
-  token_policies = ["default"]
+  path             = vault_saml_auth_backend.auth0.path
+  name             = "default"
+  token_policies   = ["default"]
+  groups_attribute = "vault-roles"
+}
+
+# External group for admins
+resource "vault_identity_group" "superadmin" {
+  name     = "superadmin"
+  type     = "external"
+  policies = ["hcp-root"]
+}
+
+data "vault_generic_secret" "saml_mount" {
+  path = "sys/auth/${local.saml_mount}"
+}
+
+resource "vault_identity_group_alias" "superadmin_alias" {
+  name           = "vault-admin"
+  mount_accessor = data.vault_generic_secret.saml_mount.data.accessor
+  canonical_id   = vault_identity_group.superadmin.id
 }
