06f69fa3ab5fcb3f3a234d23186eb5a9ea24c6e6
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-09 16:50:02 -0400

    Actually attach the VPC

--- example.tf
+++ example.tf
@@ -16,10 +16,20 @@ resource "aws_vpc" "example" {
   }
 }
 
+resource "aws_subnet" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
 resource "aws_ec2_transit_gateway" "example" {
   tags = {
     Name = "example-tgw"
   }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "example" {
+  subnet_ids         = [aws_subnet.example.id]
+  transit_gateway_id = aws_ec2_transit_gateway.example.id
+  vpc_id             = aws_vpc.example.id
 }
 
 resource "aws_ram_resource_share" "example" {
